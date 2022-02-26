54b1926a72e6dbe4b39affe4c139b2a039d4e7f4
AlexMinez <alex_minihan@outlook.com>
2022-02-26 19:16:35 +1100

    arithmetic

---
+++ index.js
@@ -0,0 +1,43 @@
+let number = 10
+
+function add() {
+    return number += 10;
+}
+function subtract() {
+    return number -= 2;
+}
+function multiply() {
+    return number *= 5;
+}
+function divide() {
+    return number /= 2;
+}
+function add(a, b) {
+    return a += b;
+}
+function subtract (a, b) {
+    return a -= b;
+}
+function multiply(a, b) {
+    return a *= b;
+}
+function divide(a, b) {
+    return a /= b;
+}
+function increment(num){
+    return num + 1;
+}
+function decrement(num){
+    return num - 1;
+}
+function makeInt(n) {
+
+   return parseInt(n,10);
+}
+
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+    
+}
+
